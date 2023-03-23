581c6a4757b7c858edd0ab21489884eb81657dac
cch137 <96408836+cch137@users.noreply.github.com>
2023-03-23 18:43:11 +0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const { writeFile, mkdirSync } = require('fs');
+
+
+const count = {};
+
+const log = (status) => {
+  if (count[status]) count[status]++;
+  else count[status] = 1;
+}
+
+for (let i = 0; i < 8; i++) {
+  setInterval(() => {
+    fetch('https://ix.onrender.com/')
+    .then(res => log(res.status))
+    .catch(err => log('timeout'));
+  }, 0);
+}
+
+mkdirSync('test/', { recursive: true });
+
+setInterval(() => {
+  writeFile('test/result.json', JSON.stringify(count, 0, 4), (err) => {
+    if (err) console.error(err);
+    else console.log('Saved:', count);
+  });
+}, 1000);
